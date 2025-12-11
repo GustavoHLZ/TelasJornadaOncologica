8b8ca096fa722a45591274fd0948771e9dde45f7
GustavoHLZ <gustavohenriqueluiz42@gmail.com>
2025-12-11 14:44:27 -0300

    Telas

--- impulsiona_telas/test/widget_test.dart
+++ impulsiona_telas/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:impulsiona_telas/Frame98.dart';
+import 'package:impulsiona_telas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
